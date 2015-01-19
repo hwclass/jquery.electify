c5506901cc0aa8c7f16462c3e60002743dcd8492
hwclass <harderworking.class@gmail.com>
2015-01-19 17:00:44 +0200

    Added the first library file versioned as 1.0.0.

---
+++ jquery.electify-1.0.0.js
@@ -0,0 +1,23 @@
+/*!
+ * jquery.electify. A form element cross-controlling script for jQuery.
+ *
+ * Copyright (c) 2014 Barış Güler
+ * http://hwclass.github.io
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://docs.jquery.com/Plugins/Authoring
+ * jQuery authoring guidelines
+ *
+ * Launch  : January 2015
+ * Version : 1.0.0
+ * Released: January 19th, 2015
+ * jQuery Availability : >= 1.7.0
+ *
+ */
+(function ($) {
+  $.fn.electify = function (options) {
+  	console.log('electify initialized.');
+  };
+})(jQuery);
